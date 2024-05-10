b2bf1eb50e66bf2055a47bb12632544c9c266b3c
Dustin L <lawson.dustin12@gmail.com>
2024-05-09 19:06:54 -0500

    edited and added more to the project data

--- data/projectData.js
+++ data/projectData.js
@@ -1,8 +1,26 @@
-const projects = [
+export const projects = [
   {
     id : 1,
-    title : "repo",
-    description : "test repo",
+    title : "First Project",
+    description : "No description",
+    lastUpdate : "1 Month ago"
+  },
+  {
+    id : 2,
+    title : "GitSub Clone",
+    description : "Group project copying the styling of github",
     lastUpdate : "5 min ago"
+  },
+  {
+    id : 3,
+    title : "Untitled-Project",
+    description : "test project",
+    lastUpdate : "30 min ago"
+  },
+  {
+    id : 4,
+    title : "Bestest Project",
+    description : "No description",
+    lastUpdate : "1 week ago"
   },
 ]
