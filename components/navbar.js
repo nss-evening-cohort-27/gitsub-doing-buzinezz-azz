54ae97ad35b1dd094226017ed0cb9314d1416069
Dustin L <lawson.dustin12@gmail.com>
2024-05-11 10:15:12 -0500

    added the icons to the navbar

--- components/navbar.js
+++ components/navbar.js
@@ -7,10 +7,10 @@ export const navStuff = () => {
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link" href="#">Overview </a>
-      <a class="nav-item nav-link" href="#">Repositories</a>
-      <a class="nav-item nav-link" href="projectsPage.html">Projects</a>
-      <a class="nav-item nav-link" href="#">Packages</a>
+      <a class="nav-item nav-link" href="#"><img class="icon" src="./assets/icons/overview.png">Overview </a>
+      <a class="nav-item nav-link" href="#"><img class="icon" src="./assets/icons/repos.png">Repositories</a>
+      <a class="nav-item nav-link" href="projectsPage.html"><img class="icon" src="./assets/icons/projects.png">Projects</a>
+      <a class="nav-item nav-link" href="packages.html"><img class="icon" src="./assets/icons/packs.png">Packages</a>
     </div>
   </div>
 </nav>
