68c750fc5205b67c4309a0b205ff7e4542b7a5bb
Nelson Ngaleumo <nelsonngaleumo@yahoo.fr>
2024-05-13 20:49:12 -0500

    added form to html

--- packages.js
+++ packages.js
@@ -1,4 +1,5 @@
-import { projectForm } from "./scripts/projectsPage";
+import { projectForm } from "./components/projectForm.js";
+import { renderToDom } from "./utils/renderToDom.js";
 const renderProForm = () => {
   let proForm = projectForm();
   console.log("rendering");
